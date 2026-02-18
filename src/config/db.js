780a2b978652b875f0f6e96de6339027dfeca336
devBuku <shubhayanbagchi30@gmail.com>
2026-02-18 11:02:17 +0530

    new start

--- src/config/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-async function connectToDb() {
-    await mongoose.connect(process.env.MONGODB_URI);
-    console.log(`MongoDB connected`);
-}
-module.exports = connectToDb;
